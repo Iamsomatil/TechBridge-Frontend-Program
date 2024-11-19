04ab79001a28cd30a6785d1c8998ac504ab59637
Akinsanya Samson <104023530+Iamsomatil@users.noreply.github.com>
2024-11-19 11:52:36 +0100

    Add files via upload
    decoder challenge/assignment to reverse the function in the code back to it's default  by our tutor

---
+++ javascript decoder/script.js
@@ -0,0 +1,51 @@
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const reverse = (alpha) => {
+  const rIndex = 25 - alphabet.indexOf(alpha);
+  return alphabet[rIndex];
+};
+
+function reverseWord(word) {
+  let wordOutput = "";
+  for (const element of word) {
+    if (alphabet.includes(element)) {
+      wordOutput += reverse(element);
+    } else {
+      wordOutput += element;
+    }
+  }
+  return wordOutput;
+}
+
+console.log(reverseWord("how are you"));
+
+const encrypted = reverseWord("how are you");
+console.log(encrypted);
+console.log(reverseWord(encrypted));
